670fb8e82f67ba90bbed2aa95aed7bb4e3cc3b70
saipendyala1 <saitejapendyala0@gmail.com>
2022-06-24 22:38:42 +0100

    Enter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const PORT = process.env.PORT || 5000;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
